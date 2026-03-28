8f06d35139fb4ff716659e2ca14d13a107ff5802
bobr_zlober <bobr.zlober@gmail.com>
2026-03-28 20:26:35 +0200

    trying smth

--- Lab2.cs
+++ Lab2.cs
@@ -4,6 +4,7 @@ class Node
 {
     public double Value;
     public Node Next;
+    public Node Prev;
     
 }
 class Program
@@ -13,6 +14,26 @@ class Program
         Console.WriteLine("waiting for n:");
         int n = int.Parse(Console.ReadLine());
         int ListSize = n*2;
-        Console.WriteLine($"List will be {ListSize} ellements");
+        Console.WriteLine($"List will be {ListSize} ellements\n1-position number: ");
+
+        Node head = new Node();
+        head.Value = double.Parse(Console.ReadLine());
+        head.Next = null;
+        head.Prev = null;
+
+        Node tail = head;
+
+        for (int i = 2; i <= ListSize; i++)
+        {
+            Console.WriteLine($"{i}-position number: ");
+            Node newNode = new Node();
+            newNode.Value = double.Parse(Console.ReadLine());
+            newNode.Next = null;
+            newNode.Prev = tail;
+            
+            tail.Next = newNode;
+            tail = newNode;
+        }
+        Node current = tail;
     }
 }
