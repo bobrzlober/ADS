8679a90a03e7e8290ad8a52a273dc0017974f4f2
bobr_zlober <bobr.zlober@gmail.com>
2026-04-12 17:19:45 +0300

    fixed some visual bugs, finished Lab3

--- Lab3/Form1.cs
+++ Lab3/Form1.cs
@@ -35,7 +35,7 @@ public partial class Form1 : Form
             for (int j = 0; j < N; j++)
             {
                 double val = rng.NextDouble() * 2.0 * k;
-                dirMatrix[i, j] = val >= 1.0 ? 1 : 0;
+                dirMatrix[i, j] = (val >= 1.0) ? 1 : 0;
             }
 
         for (int i = 0; i < N; i++)
@@ -50,54 +50,74 @@ public partial class Form1 : Form
                 Console.Write(dirMatrix[i, j] + " ");
             Console.WriteLine();
         }
+        Console.WriteLine("undir:");
+        for (int i = 0; i < N; i++)
+        {
+        for (int j = 0; j < N; j++)
+            Console.Write(undirMatrix[i, j] + " ");
+        Console.WriteLine();
+}
     }
-
-    void OnPaint(object sender, PaintEventArgs e)
+void OnPaint(object sender, PaintEventArgs e)
     {
         var g = e.Graphics;
         int r = 20;
         int offsetX = 1000;
-        for(int i = 0; i<N; i++)
+        int loopSize = 40;
+        for (int i = 0; i < N; i++)
         {
-            for(int j = 0; j<N; j++)
-            {
-                if(undirMatrix[i, j] == 1)
-                {
+            for (int j = i + 1; j < N; j++)
+                if (undirMatrix[i, j] == 1)
                     g.DrawLine(Pens.Black, coordX[i], coordY[i], coordX[j], coordY[j]);
-                }
-            }
-        }
-        for(int i = 0; i<N; i++)
-        {
-            g.FillEllipse(Brushes.White, coordX[i] - r, coordY[i] - r, r*2, r*2);
-            g.DrawEllipse(Pens.Black, coordX[i] - r, coordY[i] - r, r*2, r*2);
+            g.FillEllipse(Brushes.White, coordX[i] - r, coordY[i] - r, r * 2, r * 2);
+            g.DrawEllipse(Pens.Black, coordX[i] - r, coordY[i] - r, r * 2, r * 2);
             g.DrawString((i + 1).ToString(), this.Font, Brushes.Black, coordX[i] - 5, coordY[i] - 7);
         }
-        for (int i = 0; i<N; i++)
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j<N; j++)
+            if (dirMatrix[i, i] == 1)
             {
-                if(dirMatrix[i, j] == 1)
+                int x = coordX[i] + offsetX;
+                int y = coordY[i];
+                int lx = x - loopSize / 2;
+                int ly = y - loopSize / 2;
+                if (coordY[i] == 200) ly -= r;
+                else if (coordY[i] == 600) ly += r;
+                else if (coordX[i] == 800) lx += r;
+                else if (coordX[i] == 200) lx -= r;
+
+                g.DrawEllipse(Pens.Black, lx, ly, loopSize, loopSize);
+            }
+        }
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (i != j && dirMatrix[i, j] == 1)
                 {
-                    int MidX = (coordX[i] + coordX[j]) / 2 + offsetX;
-                    int MidY = (coordY[i] + coordY[j]) / 2;
                     double angle = Math.Atan2(coordY[j] - coordY[i], coordX[j] - coordX[i]);
-                    int arrowSize = 15;
-                    g.DrawLine(Pens.Black, coordX[i] + offsetX, coordY[i], coordX[j] + offsetX, coordY[j]);
-                    int ax1 = (int)(MidX - arrowSize * Math.Cos(angle - 0.5));
-                    int ay1 = (int)(MidY - arrowSize * Math.Sin(angle - 0.5));
-                    int ax2 = (int)(MidX - arrowSize * Math.Cos(angle + 0.5));
-                    int ay2 = (int)(MidY - arrowSize * Math.Sin(angle + 0.5));
-                    g.DrawLine(Pens.DarkRed, MidX, MidY, ax1, ay1);
-                    g.DrawLine(Pens.DarkRed, MidX, MidY, ax2, ay2);
+                    int startX = coordX[i] + offsetX + (int)(r * Math.Cos(angle));
+                    int startY = coordY[i] + (int)(r * Math.Sin(angle));
+                    int endX = coordX[j] + offsetX - (int)(r * Math.Cos(angle));
+                    int endY = coordY[j] - (int)(r * Math.Sin(angle));
+                    g.DrawLine(Pens.Black, startX, startY, endX, endY);
+                    int arrowLen = 12;
+                    g.DrawLine(Pens.Black, endX, endY, (int)(endX - arrowLen * Math.Cos(angle - 0.4)), (int)(endY - arrowLen * Math.Sin(angle - 0.4)));
+                    g.DrawLine(Pens.Black, endX, endY, (int)(endX - arrowLen * Math.Cos(angle + 0.4)), (int)(endY - arrowLen * Math.Sin(angle + 0.4)));
                 }
             }
         }
-        for (int i = 0; i<N; i++)
+        for (int i = 0; i < N; i++)
         {
-            g.FillEllipse(Brushes.White, coordX[i] + offsetX - r, coordY[i] - r, r*2, r*2);
-            g.DrawEllipse(Pens.Black, coordX[i] + offsetX- r, coordY[i] - r, r*2, r*2);
-            g.DrawString((i + 1).ToString(), this.Font, Brushes.Black, coordX[i] + offsetX - 5, coordY[i] - 7);
+            int x = coordX[i] + offsetX;
+            int y = coordY[i];
+            
+            g.FillEllipse(Brushes.White, x - r, y - r, r * 2, r * 2);
+            g.DrawEllipse(Pens.Black, x - r, y - r, r * 2, r * 2);
+            g.DrawString((i + 1).ToString(), this.Font, Brushes.Black, x - 5, y - 7);
         }
+        g.DrawString("Undir graph", this.Font, Brushes.Black, 400, 650);
+        g.DrawString("Dir graph", this.Font, Brushes.Black, 1400, 650);
+        g.DrawString("A self-loop starting and ending at the same node indicates that the object is directed at itself", this.Font, Brushes.Black, 1000, 700);
     }
 }
