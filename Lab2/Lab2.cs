ca734c3574629f0e037283ba6b189c92be3a07b6
bobr_zlober <bobr.zlober@gmail.com>
2026-04-13 08:57:16 +0300

    fixed little misunderstangding

--- Lab2/Lab2.cs
+++ Lab2/Lab2.cs
@@ -5,38 +5,34 @@ class Node
     public double Value;
     public Node Next;
     public Node Prev;
-    
 }
+
 class Program
 {
     static void Main()
     {
         Console.WriteLine("waiting for n:");
         int n = int.Parse(Console.ReadLine());
-        int ListSize = n*2;
-        Console.WriteLine($"List will be {ListSize} ellements\n1-position number: ");
-
+        Console.WriteLine("1-position number:");
         Node head = new Node();
         head.Value = double.Parse(Console.ReadLine());
         head.Next = null;
         head.Prev = null;
-
         Node tail = head;
-
-        for (int i = 2; i <= ListSize; i++)
+        for (int i = 2; i <= n + n; i++)
         {
-            Console.WriteLine($"{i}-position number: ");
+            Console.WriteLine($"{i}-position number:");
             Node newNode = new Node();
             newNode.Value = double.Parse(Console.ReadLine());
             newNode.Next = null;
             newNode.Prev = tail;
-            
             tail.Next = newNode;
             tail = newNode;
         }
         Node left = head;
         Node right = tail;
         double result = 1;
+
         while (left != right && left.Prev != right)
         {
             result *= (left.Value - right.Value);
@@ -45,7 +41,7 @@ class Program
         }
         Console.WriteLine($"result: {result}");
         Node current = head;
-        while(current != null)
+        while (current != null)
         {
             Node next = current.Next;
             current.Next = null;
